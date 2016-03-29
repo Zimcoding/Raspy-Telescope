41ba8771be0474ee8dbda7d7d5360c8dc0e51bc9
Zimcoding <ikislol@gmail.com>
2016-03-29 16:15:04 +0200

    starting implementation on heater driver

--- Raspy-Telescope/DewHeater.py
+++ Raspy-Telescope/DewHeater.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2016 Wim Rappé
-# Author: Wim Rappé
+# Copyright (c) 2016 Wim Rappe
+# Author: Wim Rappe
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,4 +19,55 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import Adafruit_BME280 as BME280
+# Import the sys for importing from another location
+import sys
+sys.path.insert(0, "../lib/Adafruit_Python_BME280")
+
+import Adafruit_BME280
+import Adafruit_MotorHAT as MtrHat
+import time
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = MtrHat.Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(MtrHat.Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(MtrHat.Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(MtrHat.Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(MtrHat.Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+################ Actual program
+DewHeater1 = mh.getMotor(1)
+Dewheater2 = mh.getMotor(2)
+
+sensor = Adafruit_BME280.BME280(mode=Adafruit_BME280.BME280_OSAMPLE_8)
+
+#Read sensor and display
+while (True):
+    degrees = sensor.read_temperature()
+    pascals = sensor.read_pressure()
+    hectopascals = pascals / 100
+    humidity = sensor.read_humidity()
+
+
+    print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+    print 'Temp      = {0:0.3f} deg C'.format(degrees)
+    print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+    print 'Humidity  = {0:0.2f} %'.format(humidity)
+
+    #Start heater if humidity is to high
+    if humidity > 80:
+        print "Starting to heat..."
+        DewHeater1.run(MtrHat.Adafruit_MotorHAT.FORWARD)
+        DewHeater1.setSpeed(int((humidity-80)*10))
+    else:
+        DewHeater1.run(MtrHat.Adafruit_MotorHAT.RELEASE)
+        print "Not heating"
+    time.sleep(1)
+
+    
+    
